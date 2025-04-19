177006a4caa88e3318979aab1f55c35acd9bfa06
akos011221 <orbanakos2001@gmail.com>
2025-04-19 13:26:05 +0200

    refactor(api,deploy,pkg): Remove parts of the previous NSM

--- pkg/controller/controller.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "k8s.io/client-go/kubernetes"
-
-// Controller is the controller implementation for NetworkService and NetworkConnection resources
-type Controller struct {
-	// Standard kubernetes clientset
-	kubeClientset kubernetes.Interface
-}
